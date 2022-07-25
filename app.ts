136177d06095075c5a46d0b1738501ed7299d7df
Danny <leonel77.3@gmail.com>
2022-07-25 13:38:15 -0400

    Update app.js

--- app.ts
+++ app.ts
@@ -7,8 +7,8 @@ const app = express();
 const port = 3000;
 
 // JSON Parser
+// app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', router);
 
