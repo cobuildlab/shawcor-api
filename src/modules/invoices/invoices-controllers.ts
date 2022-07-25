9a5b5c392530736fb775bee503d9e3f27ce9d400
Danny <leonel77.3@gmail.com>
2022-07-25 12:54:23 -0400

    Log request

--- src/modules/invoices/invoices-controllers.ts
+++ src/modules/invoices/invoices-controllers.ts
@@ -3,14 +3,7 @@ import expressAsyncWrapper from '../../shared/utils';
 
 export const syncInvoiceToEnverus = expressAsyncWrapper(
   async (request, response) => {
-    console.log('request.body', JSON.stringify(request.body, null, 2));
-    log(
-      `syncInvoiceToEnverus request.body: ${JSON.stringify(
-        request.body,
-        null,
-        2,
-      )}`,
-    );
+    log(`syncInvoiceToEnverus request: ${JSON.stringify(request, null, 2)}`);
 
     await flush();
     return response.status(200).json({ message: 'Invoice synced to Enverus' });
